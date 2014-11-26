feb8635592dc1f1150af9a5d461f70b812c8d0b1
Luis Javier Collazos <ljcollazos@gmail.com>
2014-11-26 14:15:35 +0100

    app inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MiblogApp::Application.config.secret_key_base = 'd95adb6aba519f5bc84efab4f6d93ff5fcc4ea4652ac22a640bff30fbc72dda72f14a4e8c3f5cbdd041a748d1a4c1ac369691638eccb92fa54cb90f6d2665bdf'
